e929594648fcc7425762b1764816ca2a49da91ab
unknown <sara.moradi54@gmail.com>
2024-02-19 23:59:16 +0330

    create Github Pages

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base : '/neozarin/',
+    base : '/neozarrin/',
   plugins: [
     vue(),
     vueJsx(),
